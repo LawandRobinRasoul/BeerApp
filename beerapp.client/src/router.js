36638106cd8bad09e7272e64f866cf5e4de14bc3
Lawand Rasoul <lawand.rasoul@qinshift.com>
2025-03-24 09:37:27 +0100

    Beers view added

--- beerapp.client/src/router.js
+++ beerapp.client/src/router.js
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 
 import HomeView from './views/HomeView.vue'
+import BeersView from './views/BeersView.vue'
 
 const routes = [
   { path: '/', component: HomeView },
+  { path: '/beers', component: BeersView }
 ]
 
 const router = createRouter({
