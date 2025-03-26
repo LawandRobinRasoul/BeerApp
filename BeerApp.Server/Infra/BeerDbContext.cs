a5e5c69176dc04a0d540e213873a575657e0e507
Lawand Rasoul <lawand.rasoul@qinshift.com>
2025-03-26 15:19:13 +0100

    dbcontext

--- BeerApp.Server/Infra/BeerDbContext.cs
+++ BeerApp.Server/Infra/BeerDbContext.cs
@@ -1,5 +1,20 @@
-public class ProductsDbContext : DbContext
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+
+public class BeerDbContext : DbContext
 {
-    public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+    public BeerDbContext(DbContextOptions<BeerDbContext> options)
         : base(options) { }
+
+    public DbSet<BeerDto> Beers { get; set; }
+    public DbSet<ReviewDto> Reviews { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ReviewDto>()
+            .HasOne(r => r.Beer)
+            .WithMany(b => b.Reviews)
+            .HasForeignKey(r => r.BeerId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
---
+++ BeerApp.Server/Migrations/20250326140404_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    imageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BeerId = table.Column<int>(type: "int", nullable: false),
+                    Reviewer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BitternesScore = table.Column<int>(type: "int", nullable: false),
+                    FruitinessScore = table.Column<int>(type: "int", nullable: false),
+                    SweetnessScore = table.Column<int>(type: "int", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Beers_BeerId",
+                        column: x => x.BeerId,
+                        principalTable: "Beers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_BeerId",
+                table: "Reviews",
+                column: "BeerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Beers");
+        }
+    }
+}
