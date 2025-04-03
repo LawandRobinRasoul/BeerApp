963a79fbe1140f7bdef6dfd8be257122cf7715ab
Lawand Rasoul <lawand.rasoul@qinshift.com>
2025-04-03 15:12:19 +0200

    Interface for beer repo added. BeerService added in core folder

---
+++ BeerApp.Server/Core/BeerService.cs
@@ -0,0 +1,18 @@
+﻿namespace BeerApp.Server.Core
+{
+    public class BeerService
+    {
+        private readonly IBeerRepository _beerRepository;
+        public BeerService(IBeerRepository beerRepository)
+        {
+            _beerRepository = beerRepository ?? throw new ArgumentNullException(nameof(beerRepository));
+        }
+
+
+
+        public async Task<List<BeerDto>> GetBeersAsync()
+        {
+            return await _beerRepository.GetBeersAsync();
+        }
+    }
+}
---
+++ BeerApp.Server/Core/IBeerRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace BeerApp.Server.Core
+{
+    public interface IBeerRepository
+    {
+        Task<List<BeerDto>> GetBeersAsync();
+        Task<BeerDto> GetBeerByIdAsync(int id);
+        Task<List<ReviewDto>> GetReviewsByBeerIdAsync(int beerId);
+        Task AddBeerAsync(BeerDto beer);
+        Task UpdateBeerAsync(BeerDto beer);
+        Task DeleteBeerAsync(int id);
+        Task AddReviewAsync(ReviewDto review);
+        Task UpdateReviewAsync(ReviewDto review);
+        Task DeleteReviewAsync(int id);
+    }
+}
