7eafbb8b16cc939705dfe931c55f176d681d8aec
LawandRobinRasoul <lawand@live.se>
2025-04-11 08:41:05 +0200

    db migration

---
+++ BeerApp.Server/Migrations/20250411062517_AddCreatedAt.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeerApp.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Reviews",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "Reviews",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Beers",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Beers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Beers");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAt",
+                table: "Beers",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldDefaultValueSql: "GETUTCDATE()");
+        }
+    }
+}
